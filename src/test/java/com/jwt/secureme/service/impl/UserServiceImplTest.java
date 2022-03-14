ca7fd8fa6ded7ffaa537e2f55f7ec9306cae5cad
Sweelam <mohamed_bob2007@yahoo.com>
2022-03-14 11:07:50 +0400

    Add service layer tests

--- src/test/java/com/jwt/secureme/service/impl/UserServiceImplTest.java
+++ src/test/java/com/jwt/secureme/service/impl/UserServiceImplTest.java
@@ -11,8 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
