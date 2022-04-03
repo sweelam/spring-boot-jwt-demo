ca0462a6ffb8c27d9a736fe78c53f31b490daeda
msweelam <Sw33l@m_2021>
2022-04-03 13:32:22 +0400

    update swagger config with bearer security

--- src/main/java/com/jwt/secureme/SecureMeApplication.java
+++ src/main/java/com/jwt/secureme/SecureMeApplication.java
@@ -3,24 +3,34 @@ package com.jwt.secureme;
 import com.jwt.secureme.dto.AdminDetails;
 import com.jwt.secureme.service.UserService;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import static com.jwt.secureme.config.SecurityConfig.APPLICATION_NAME;
+
 @SpringBootApplication
 @RequiredArgsConstructor
 @EnableConfigurationProperties(AdminDetails.class)
-@OpenAPIDefinition(info = @Info(
+@OpenAPIDefinition(
+        info = @Info(
         description = "jwt-auth demo using springboot",
         version = "1.0",
-        title = "secure-me"
-))
-@Slf4j
+        title = "secure-me")
+)
+@SecurityScheme(
+        name = APPLICATION_NAME,
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        in = SecuritySchemeIn.HEADER
+)
 public class SecureMeApplication implements ApplicationRunner {
 
     private final UserService userService;
